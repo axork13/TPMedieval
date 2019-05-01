85e1cd9687b04b17da39c7e55e66568cbaafd2c5
Jimmy Marmin <jimmy.marmin@gmail.com>
2019-05-01 18:52:48 +0200

    Ajout de la classe Paysan

---
+++ TPMedieval/TPMedieval/Classes/Paysan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPMedieval.Classes
+{
+    public class Paysan : Villageois
+    {
+        private int cout;
+        public Paysan(string n, string p, int a, int cout) : base(n, p, a)
+        {
+            this.cout = cout;
+        }
+    }
+}
