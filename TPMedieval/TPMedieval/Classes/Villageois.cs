2d6288f002d76c22bd69335ae792afbb394e6aca
Jimmy Marmin <jimmy.marmin@gmail.com>
2019-05-01 18:43:31 +0200

    Création de la classe abstraite Villageois

---
+++ TPMedieval/TPMedieval/Classes/Villageois.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPMedieval.Classes
+{
+    public abstract class Villageois
+    {
+        private string nom;
+        private string prenom;
+        private int age;
+
+        protected Villageois(string nom, string prenom, int age)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.age = age;
+        }
+    }
+}
